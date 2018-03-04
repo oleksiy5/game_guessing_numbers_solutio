f0d385a9fd26e41914f25737c34831a8e61fc311
Oleksiy Nekrasov <n.507592979@gmail.com>
2018-03-04 18:02:10 +0100

    upload my project for learn

---
+++ game_guessing_numbers/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace game_guessing_numbers
+{
+    class Program
+    {
+        static int _winNumber;
+
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            _winNumber = random.Next(0, 10);
+            
+            string command = string.Empty;
+            string gameResult = string.Empty; 
+            while(command != "end")
+            {
+                command = Console.ReadLine();
+                if(command == _winNumber.ToString())
+                {
+                    gameResult = "win";
+                    break;
+                }
+            }
+
+            if(gameResult == "win")
+            {
+                Console.WriteLine("Gratulajce wygrałeś!");
+                Console.WriteLine(
+                    string.Format("To była liczba {0}", _winNumber));
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Przykro mi przegrałeś.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
